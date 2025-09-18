982073a364478ca3ea9b1b023e6f78797c5e32c0
Andrew Lapp <andrew@github.rew.la>
2025-09-18 15:55:40 -0400

    fix multi-node nccl

--- owl_data/mp4_to_latent.py
+++ owl_data/mp4_to_latent.py
@@ -206,12 +206,12 @@ def run_multinode_encode_and_save(
     assert vae_cfg_path and vae_ckpt_path, "Provide --vae_cfg_path and --vae_ckpt_path"
 
     local_rank = int(os.environ["LOCAL_RANK"])
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(local_rank)
-    torch.cuda.set_device(0)
-    device = torch.device("cuda", 0)
+    torch.cuda.set_device(local_rank)
+    device = torch.device("cuda", local_rank)
 
     # Now initialize DDP
-    dist.init_process_group(backend="nccl", init_method="env://")
+    opts = dist.NCCLBackendOptions(device_id=local_rank)
+    dist.init_process_group(backend="nccl", init_method="env://", pg_options=opts)
     rank = dist.get_rank()
 
     model = BatchedEncodingPipe(vae_cfg_path, vae_ckpt_path, dtype=torch.float16)
